5c63b4e059b046c70c4cd20a5533d79ba5834ddf
WallnerArt <eugenwallner.design@gmail.com>
2024-04-09 17:14:24 +0200

    homework_06

---
+++ FrontTestGit/Code/Lesson_07/lesson_07.js
@@ -0,0 +1,31 @@
+//Вывод в консоль информации
+//console.log('hello world 333')
+
+//Создание Переменной
+let s ='hello';
+s = 1;
+
+//Создание Константы
+const myConst = 111;
+//myConst = 2;
+
+//Создание Переменной через var ( так писать не рекомендуется)
+var myVariable = 333;
+
+// переменная числового типа
+let a = 1;
+
+// переменная строкового типа
+let b = 'hello';
+
+// переменная булевого типа
+let c = true;
+
+let first = 1;
+let second = first;
+
+first = 2;
+
+console.log(first);
+console.log(second);
+console.log();
