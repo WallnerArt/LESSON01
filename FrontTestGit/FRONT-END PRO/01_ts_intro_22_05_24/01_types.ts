efc8141b075d0ee6b2f6a6e51e204437cc91d145
Eugen Wallner <eugenwallner.design@gmail.com>
2024-05-22 17:14:00 +0200

    01_typesUrok

--- FrontTestGit/FRONT-END PRO/01_ts_intro_22_05_24/01_types.ts
+++ FrontTestGit/FRONT-END PRO/01_ts_intro_22_05_24/01_types.ts
@@ -1,25 +1,42 @@
 let isLoading: boolean;
 isLoading = true;
 isLoading = false;
+
 //NUMBER  
 //isLoading = 42; --ошибка типизации
 
-let num: number | string = 42;
+let num: number | string = 42;  // Логическое ИЛИ
 num = 'hello';
 
+
+// STRING
+// let str: 'hello world!' | 'Goodbye!' = 'hello world!';
 const str = 'hello world!';
 console.log(str);
 
-//ARRAY 
+// ARRAY 
+
 let primes1 = [2, 3, 5, 7];
 const primes2 = [11, 13, 17, 19]
 
 primes2.push(123); // [11, 13, 17, 19, 123]
-primes2.forEach((e, i) => console.log(e));
+primes2.forEach((e: number, i: number) => console.log(e, i));
 
 //FUNCTION
 const sum = (a: number, b: number): number => a + b;
 console.log(sum(10, 5));
+
+//ARRAY
+let primes3: (number | string)[] = [2, 3, 5, 7, 'qwerty'];
+let primes4: number[] | string[] | {age: number}[] = [2, 3, 5, 7];
+const primes5: number [] = [2, 3, 5, 7];
+primes5.forEach((e: number, i: number): void => console.log(e));
+let primes6: number [] | string [] | {age: number} [] = [{age: 20}, {age: 15}, {age: 3}];
+let primes7: (number | string | {age: number}) [] = [{age: 20}, 12, '34'];
+
+if (typeof(primes7[0]) == "object") {
+    primes7[0].age = 17;
+}
 
 
 
