959668d405d9ce2d302606dc8cea39ffb2dcc344
Eugen Wallner <eugenwallner.design@gmail.com>
2024-06-23 11:51:47 +0200

    renamed

---
+++ FrontTestGit/-FRONTEND-/02_react_intro_21_06_2024/scriptCC.js
@@ -0,0 +1,14 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+class Cohort40FS extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Cohort40FS</h1>
+        <h1 className="text-center">Cohort40FS</h1>
+      </div>
+    );
+  }
+}
+
+root.render(<Cohort40FS />)
